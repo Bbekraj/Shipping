d28284226f0954eaac590656837b55fd487b21a5
Bbekraj <86979229+Bbekraj@users.noreply.github.com>
2021-07-06 15:50:30 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+
+$(window).scroll(function(){
+    if($(this).scrollTop()>100){
+        $(".header").addClass("sticky")
+        $(".array").show()
+    }
+    else{
+        $(".header").removeClass("sticky")
+        $(".array").hide()
+    }
+})
+var myCarousel = document.querySelector('#carouselExampleSlidesOnly')
+var carousel = new bootstrap.Carousel(myCarousel);
